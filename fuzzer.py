59b579199c6e50aa97cf028c9864a29153640a0f
david <david@kali.dynamic.voo.be>
2024-12-25 20:56:12 +0100

    get method added

--- fuzzer.py
+++ fuzzer.py
@@ -3,10 +3,13 @@ import requests
 import re
 import time
 import argparse
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url")
 parser.add_argument("payloads")
+parser.add_argument("method")
+parser.add_argument("get_parameter", nargs="?") # nargs because this argument is optionnal
 args = parser.parse_args()
 
 wordlist = open(args.payloads, 'r')
@@ -14,14 +17,46 @@ payloads = wordlist.readlines()
 wordlist.close()
 
 url = args.url
+method = args.method
 r = requests.get(url)
-params = re.findall('(?<=name=")\w+(?=")', r.text) 
+timenow = str(datetime.now()).replace(" ", "_")
+filename = "./output/fuzzing_result_" + timenow + ".txt"
+f = open(filename, "a")
 
-for payload in payloads:
-    payload = payload[0:len(payload)-1]
-    for param in params:
-        p = requests.post(url, data={param: payload})
-        print(str(p.status_code) + " " + str(len(p.text)) + " " + str(p.elapsed.total_seconds()) + " " + url + "?" + param + "=" + payload)
-        time.sleep(1)
+# handle fuzzing via POST method
+if method == "post":
+    params = re.findall('(?<=name=")\w+(?=")', r.text) 
+    n = 0
+    for payload in payloads:
+        n = n + 1
+        payload = payload[0:len(payload)-1]
+        for param in params:
+            p = requests.post(url, data={param: payload})
+            result = str(p.status_code) + " " + str(len(p.text)) + " " + str(p.elapsed.total_seconds()) + " " + url + "?" + param + "=" + payload
+            print(str(n) + " out of  " + str(len(payloads)) + " - " + result)
+            time.sleep(1)
+            f.write(result + "\n")
+    f.close()
+    print("Results stored in output folder")
+
+# handle fuzzing via url (e.g. directory listing ...)
+elif method == "get":
+    get_parameter = args.get_parameter
+    if get_parameter:
+        n = 0
+        for payload in payloads:
+            n = n + 1
+            payload = payload[0:len(payload)-1]
+            g = requests.get(url + "?" + get_parameter + "=" + payload)
+            result = str(g.status_code) + " " + str(len(g.text)) + " " + str(g.elapsed.total_seconds()) + " " + url + "?" + get_parameter + "=" + payload
+            print(str(n) + " out of  " + str(len(payloads)) + " - " + result)
+            f.write(result + "\n")
+        f.close()
+        print("Results stored in output folder")
+    else:
+        print("Please specify a parameter name")
+
+else:
+    print("Choose a method 'post' or 'get'")
 
 
